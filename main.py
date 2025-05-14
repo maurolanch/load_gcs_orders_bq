4a9a383ed6f3037e11fd6cedc95ef91173c38134
Mauricio L <maurolanm@gmail.com>
2025-05-14 13:04:26 -0500

    Logging fixed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from snapshot_processor import process_snapshots
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 @app.route("/", methods=["POST"])
 def handle_request():
+    logging.info("Received POST request.")
     return process_snapshots(request)
